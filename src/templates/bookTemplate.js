ddd1215e0c95deb2dfb1f6b87df1ba46225f9147
Michael Heffernan <fitflex.platform@gmail.com>
2019-12-16 21:03:16 -0500

    Fixed issue rendering images on books page

--- src/templates/bookTemplate.js
+++ src/templates/bookTemplate.js
@@ -1,18 +1,40 @@
 import React from "react"
 import Layout from "../components/layout"
 import BookItem from "../components/BookItem"
+import { graphql } from "gatsby"
 
 const BookTemplate = props => {
+  console.log(props.data)
   return (
     <Layout>
       <BookItem
-        bookCover={props.pageContext.localImage.childImageSharp.fixed}
-        authorName={props.pageContext.author.name}
-        bookSummary={props.pageContext.summary}
-        bookTitle={props.pageContext.title}
+        bookCover={props.data.book.localImage.childImageSharp.fixed}
+        authorName={props.data.book.author.name}
+        bookSummary={props.data.book.summary}
+        bookTitle={props.data.book.title}
       />
     </Layout>
   )
 }
+
+export const query = graphql`
+  query BookQuery($bookId: String!) {
+    book(id: { eq: $bookId }) {
+      summary
+      title
+      localImage {
+        childImageSharp {
+          fixed(width: 200) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      id
+      author {
+        name
+      }
+    }
+  }
+`
 
 export default BookTemplate
