9a087d9db0b7785ff4e92efb236c2cc399c5733b
Michael Heffernan <fitflex.platform@gmail.com>
2019-12-19 20:56:56 -0500

    adding firebase configurations

---
+++ src/components/Firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBI7VLdJXkd56OY43LEP8SjRHj1pBIpPCY",
+  authDomain: "gatsby-book-club-26a7b.firebaseapp.com",
+  databaseURL: "https://gatsby-book-club-26a7b.firebaseio.com",
+  projectId: "gatsby-book-club-26a7b",
+  storageBucket: "gatsby-book-club-26a7b.appspot.com",
+  messagingSenderId: "1055963427641",
+  appId: "1:1055963427641:web:f3fe9e4fb885f41f4feee8",
+  measurementId: "G-W52T3KLZ3K",
+}
+
+export default firebaseConfig
