7a88fc7328831e4429381f876e126896a390e452
Michael Heffernan <fitflex.platform@gmail.com>
2019-12-24 16:47:58 -0500

    created new login page

---
+++ src/pages/login.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react"
+import { Link } from "gatsby"
+import { useAuth } from "../components/Firebase"
+
+import Layout from "../components/layout"
+
+const SecondPage = () => {
+  const [formValues, setFormValues] = useState({ email: "", password: "" })
+  const { firebase } = useAuth()
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    firebase.login({ email: formValues.email, password: formValues.password })
+  }
+
+  function handleInputChange(e) {
+    e.persist()
+    setFormValues(currentValues => ({
+      ...currentValues,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  return (
+    <Layout>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={formValues.email}
+          name="email"
+          onChange={handleInputChange}
+          placeholder="email"
+          type="email"
+        />
+        <input
+          value={formValues.password}
+          name="password"
+          onChange={handleInputChange}
+          placeholder="password"
+          type="password"
+        />
+        <button type="submit">Login</button>
+      </form>
+    </Layout>
+  )
+}
+
+export default SecondPage
