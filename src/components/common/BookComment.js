54009a16e66ac152d04ffa2165bda2bf8ff230d2
Michael Heffernan <michael.heffernan.j@gmail.com>
2020-01-14 06:58:34 -0500

    Edited post comments and added firebase functions capability

--- src/components/common/BookComment.js
+++ src/components/common/BookComment.js
@@ -1,4 +1,32 @@
 import React, { useEffect, useState } from "react"
+import styled from "styled-components"
+import { Button } from "./Button"
+import { Input } from "./Input"
+
+const CommentForm = styled.form`
+  display: flex;
+  margin-top: 32px;
+
+  ${Input} {
+    margin-right: 8px;
+    margin-top: auto;
+    margin-bottom: auto;
+  }
+
+  ${Button} {
+    margin: auto 0;
+  }
+`
+
+const CommentListItem = styled.div`
+  > strong {
+    font-size: 80%;
+    color: #666;
+  }
+
+  border-bottom: 1px solid #ddd;
+  padding: 4px 0;
+`
 
 export const BookComments = ({ firebase, bookId }) => {
   const [comments, setComments] = useState([])
@@ -28,11 +56,15 @@ export const BookComments = ({ firebase, bookId }) => {
 
   return (
     <div>
+      <CommentForm>
+        <Input />
+        <Button>Post Comment</Button>
+      </CommentForm>
       {comments.map(comment => (
-        <div key={comment.id}>
+        <CommentListItem key={comment.id}>
           <strong>{comment.username}</strong>
           <div>{comment.text}</div>
-        </div>
+        </CommentListItem>
       ))}
     </div>
   )
--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -6,6 +6,7 @@ export const Button = styled.button`
   color: white;
   border-radius: 4px;
   cursor: pointer;
+  white-space: nowrap;
   ${props => (props.block ? "display: block; width: 100%;" : "")}
 
   &:hover {
